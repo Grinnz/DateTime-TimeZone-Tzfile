1b3c497ed6e1b4f46d5f2f4e8bfdce4d3ef44cf6
Zefram <zefram@fysh.org>
2007-01-30 01:49:49 +0000

    more parsing

--- lib/DateTime/TimeZone/Tzfile.pm
+++ lib/DateTime/TimeZone/Tzfile.pm
@@ -25,6 +25,13 @@ sub _read_s32($) {
 
 sub _read_u8($) { ord(_saferead($_[0], 1)) }
 
+use constant UNIX_EPOCH_RDN => 719163;
+
+sub _read_tm32($) {
+	my $t = _read_s32($_[0]);
+	return [ UNIX_EPOCH_RDN + fdiv($t, 86400), fmod($t, 86400) ];
+}
+
 sub new($$) {
 	my($class, $filename) = @_;
 	croak "filename must be absolute" unless $filename =~ m#\A/#;
@@ -36,14 +43,10 @@ sub new($$) {
 	_saferead($fh, 15);
 	my($ttisgmtcnt, $ttisstdcnt, $leapcnt, $timecnt, $typecnt, $charcnt) =
 		map { _read_u32($fh) } 1 .. 6;
-	my @obs_times = map { _read_s32($fh) } 1 .. $timecnt;
+	my @obs_times = map { _read_tm32($fh) } 1 .. $timecnt;
 	my @obs_types = map { _read_u8($fh) } 1 .. $timecnt;
 	my @types = map {
-		{
-			gmtoff => _read_s32($fh),
-			isdst => _read_u8($fh),
-			abbrind => _read_u8($fh),
-		}
+		[ _read_s32($fh), _read_u8($fh), _read_u8($fh) ]
 	} 1 .. $typecnt;
 	my $chars = _saferead($fh, $charcnt);
 	for(my $i = $leapcnt; $i--; ) { _saferead($fh, 8); }
@@ -53,7 +56,21 @@ sub new($$) {
 		# TODO: read 64-bit version of same data
 	}
 	$fh = undef;
-	die "TODO";
+	die "TODO: check that obs_times are sorted";
+	die "TODO: turn obs_types into references to types";
+	die "TODO: unshift first standard type onto obs_types";
+	foreach my $type (@types) {
+		my $abbrind = $type->[2];
+		croak "can't parse tzfile: invalid abbreviation index"
+			if $abbrind > $charcnt;
+		pos($chars) = $abbrind;
+		$chars =~ /\G([^\0]*)/g;
+		$type->[2] = $1;
+	}
+	return bless({
+		obs_times => \@obs_times,
+		obs_types => \@obs_types,
+	}, $class);
 }
 
 1;
